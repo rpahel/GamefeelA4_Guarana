d58e04771f044bb156cbe173a747aac3de6bc25b
SamM <61594176+SamMzen@users.noreply.github.com>
2023-11-16 10:57:12 +0100

    ScreenShake

---
+++ Assets/Scripts/Shake.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shake : MonoBehaviour {
+    public bool start = false;
+    public float duration = 1f;
+    public AnimationCurve curve;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update() { 
+        if (start) {
+            start = false;
+            StartCoroutine(Shaking());
+        }
+    }
+
+    IEnumerator Shaking()
+        {
+            Vector3 startPosition = transform.position;
+            float elapsedTime = 0f;
+
+            while (elapsedTime < duration)
+            {
+                elapsedTime += Time.deltaTime;
+                float strenght = curve.Evaluate(elapsedTime / duration);
+                transform.position = startPosition + Random.insideUnitSphere * strenght;
+                yield return null;
+            }
+            transform.position -= startPosition;
+    }
+
+}
